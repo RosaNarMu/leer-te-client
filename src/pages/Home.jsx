98f95c69bb5c14f540b20ec4fea89d6173dee0dc
RosaNM <rosanmn.n@gmail.com>
2021-12-29 14:07:27 +0100

    home main div style navLink

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
+import { NavLink, Outlet, Link, useNavigate } from 'react-router-dom'
 
-export default function Home() {
+export default function Home({ user }) {
     return (
         <main className='home-div-main'>
+
+
+            <div className='home-div-left'>
+
+
+
+                <NavLink
+
+                    to="/display"
+                >
+                    <button className='home-div-button btn'> ¡Explora!<i class="fab fa-readme"></i></button>
+                </NavLink>
+
+            </div>
             <div className='home-div-title'>
                 <h2>¿Qué te apetece hoy?</h2>
                 <h2>¿Leer?</h2>
                 <h2>¿Escribir?</h2>
             </div>
-
-            <div className='home-div-left'>
-
-                <button className='home-div-button btn'> ¡Explora!<i class="fab fa-readme"></i></button>
-
-
-            </div>
             <div className='home-div-right'>
-                <button className='home-div-button btn'> ¡Crea! <i class="fas fa-feather-alt"></i></button>
+
+                {user ? (
+
+                    <NavLink
+                        className=''
+                        to="/create"
+                    >
+                        <button className='home-div-button btn'> ¡Crea! <i class="fas fa-feather-alt"></i></button>
+                    </NavLink>
+
+                ) : (
+                    <NavLink
+                        className=''
+                        to="/login"
+                    >
+                        <button className='home-div-button btn'> ¡Crea! <i class="fas fa-feather-alt"></i></button>
+                    </NavLink>
+                )}
+
+                {/* 
+                <NavLink
+
+                    {user ? (to = "/create") : (to = '/login')}
+                >
+                    <button className='home-div-button btn'> ¡Crea! <i class="fas fa-feather-alt"></i></button>
+                </NavLink> */}
+
 
             </div>
-        </main>
+        </main >
     )
 }
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -45,7 +45,7 @@ export default function AppRouter() {
         <BrowserRouter>
             <Navbar user={user} logout={logout} />
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home user={user} />} />
 
 
                 <Route path="contact" element={<ContactForm />} />
