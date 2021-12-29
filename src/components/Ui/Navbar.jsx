2aa87270e23a2c4d2987a65d647e951b4a362ac8
RosaNM <rosanmn.n@gmail.com>
2021-12-29 11:47:27 +0100

    navbar styled

--- src/components/Ui/Navbar.jsx
+++ src/components/Ui/Navbar.jsx
@@ -11,73 +11,97 @@ export const Navbar = ({ user, logout }) => {
     }
     return (
         <>
-            <nav className="bar" >
+            <header>
+                <nav className="navbar-main" >
+                    <ul>
+                        <li>
+                            <NavLink
+                                className=''
+                                to="/"
+                            >
+                                {<div className='logo-74x75'></div>}
+                                {/* <img src="../../assets/LOGO.png" alt="logo" /> */}
 
-                <NavLink
-                    className='link '
-                    to="/"
-                >
-                    {/*  <div className='logo'></div> */}
-                    <img src="../../assets/LOGO.png" alt="logo" />
+                            </NavLink>
 
-                </NavLink>
+                        </li>
+                    </ul>
 
-                <NavLink
-                    className='link'
-                    to="/display"
-                >
-                    <button className='btn'>¡A leer!</button>
-                </NavLink>
-
-                {user && (
-                    <NavLink
-                        className='link'
-                        to="/create"
-                    >
-                        <button className='btn'>¡A escribir!</button>
-                    </NavLink>
-                )}
-
-
-                <NavLink
-                    className='link'
-                    to="/contact"
-                >
-                    <button className='btn'>Contacto</button>
-                </NavLink>
-
-                <NavLink
-                    className='link'
-                    to="/faq"
-                >
-                    <button className='btn'>FAQ</button>
-                </NavLink>
-
-                {!user && (
-                    <NavLink
-                        className='link'
-                        to="/login"
-                    >
-                        <button className='btn'>Tu cuenta</button>
-                    </NavLink>
-                )}
+                    <ul className='navbar-center'>
+                        <li>
+                            <NavLink
+                                className=''
+                                to="/display"
+                            >
+                                <button className='btn'>¡A leer!</button>
+                            </NavLink>
+                        </li>
 
 
-                {user && (
-                    <>
-                        <NavLink
-                            className='link'
-                            to="/userprofile"
-                        >
-                            <button className='btn'>Tu cuenta</button>
-                        </NavLink>
 
-                        <button className='btn' onClick={exit}>Logout</button>
-                    </>
-                )}
 
-            </nav>
+                        {user && (
+                            <li>
+                                <NavLink
+                                    className=''
+                                    to="/create"
+                                >
+                                    <button className='btn'>¡A escribir!</button>
+                                </NavLink>
+                            </li>
+                        )}
 
+                        <li>
+                            <NavLink
+                                className=''
+                                to="/contact"
+                            >
+                                <button className='btn'>Contacto</button>
+                            </NavLink>
+                        </li>
+
+                        <li>
+
+                            <NavLink
+                                className=''
+                                to="/faq"
+                            >
+                                <button className='btn'>FAQ</button>
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                    <ul className='navbar-end'>
+                        {!user && (
+                            <li>
+                                <NavLink
+                                    className=''
+                                    to="/login"
+                                >
+                                    <button className='btn'>Tu cuenta</button>
+                                </NavLink>
+                            </li>
+                        )}
+
+
+                        {user && (
+                            <>
+                                <li>
+                                    <NavLink
+                                        className=''
+                                        to="/userprofile"
+                                    >
+                                        <button className='btn'>Tu cuenta</button>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <button className='btn' onClick={exit}>Logout</button>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </nav>
+            </header>
             <section>
                 <Outlet></Outlet>
             </section>
